5d29bc669a2d208abac307051d17e459d1e63d03
Howard Lee <howardlee@Howards-MBP.local>
2022-06-08 08:05:40 +0800

    fixed issues with home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,7 @@
 import { useContext } from 'react'
 import { Row, Col, BackTop } from 'antd'
-import Profile from '../../Components/Profile/Profile'
-import WelcomeMessage from '../../Components/WelcomeMessage/WelcomeMessage'
-import TechStack from '../../Components/TechStack/TechStack'
+import TitleMessage from '../../Components/TitleMessage/TitleMessage'
+import ThreeDCarousel from '../../Components/ThreeDCarousel/ThreeDCarousel'
 import { Fade } from 'react-awesome-reveal'
 import { MenuContext } from '../../Components/MenuProvider/MenuProvider'
 import './Home.scss'
@@ -13,42 +12,64 @@ const Home = () => {
   return (
     <div className='home-page'>
       <Fade>
-        <WelcomeMessage />
+        <TitleMessage
+          title={
+            <>
+              Hello <br /> I'm Howard.
+            </>
+          }
+        />
       </Fade>
       <Row align='middle' justify='center' id='profile-container'>
         <Col>
-          <Profile message='I am a' />
-          <Profile />
-          <Profile />
-          <Profile />
+          <TitleMessage title='I am a' />
+          <TitleMessage />
         </Col>
       </Row>
       <Fade fraction={0.4}>
-        <Profile
-          message={
+        <TitleMessage
+          title={
             <>
               Mid-Career <br /> Switcher
             </>
           }
+          message='4 years at Facebook, I had a blast with the Event Operations team in Singapore.'
         />
-        <Profile
-          message={
+        <TitleMessage
+          title={
             <>
               Bootcamp <br /> Graduate
             </>
           }
+          message='6 months at General Assembly Singapore, I learnt the ropes.'
         />
-        <Profile message={<>Blockchain ...</>} />
-        <Profile message={<>and a ...</>} />
-        <Profile
-          message={
+        <TitleMessage
+          title={
             <>
-              Full-stack <br /> Developer
+              Developer <br /> ...
             </>
           }
+          message='I really am.'
+        />
+        <TitleMessage
+          title={<>Full Stack</>}
+          message='MongoDB • Express • React • Node'
+        />
+        <TitleMessage title={<>and ...</>} />
+        <TitleMessage
+          title={
+            <>
+              Blockchain <br /> Developer
+            </>
+          }
+          message='Solidity • Hardhat • Ethers '
         />
       </Fade>
-      <TechStack />
+      <Row justify='center' align='middle' id='tech-stack'>
+        <Col>
+          <ThreeDCarousel />
+        </Col>
+      </Row>
       {menu.isMenuOpen ? <></> : <BackTop />}
     </div>
   )
