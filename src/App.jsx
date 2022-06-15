a6a3beb803d0a846d7ebe0a50b0ebfd505a3cf4d
Howard Lee <howardlee@Howards-MBP.local>
2022-06-15 22:42:53 +0800

    removed unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import { Suspense } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import { LoadingOutlined } from '@ant-design/icons'
-import { Spin } from 'antd'
 import MenuProvider from './Components/MenuProvider/MenuProvider'
 import AppLayout from './Components/AppLayout/AppLayout'
 import Home from './Pages/Home/Home'
 import './App.scss'
 
 const App = () => {
-  const loadingIcon = <LoadingOutlined />
-
   return (
     <div className='App'>
       <MenuProvider>
         <BrowserRouter>
-          {/* <Suspense fallback={<Spin indicator={loadingIcon} />}> */}
           <AppLayout>
             <Home />
           </AppLayout>
-          {/* </Suspense> */}
         </BrowserRouter>
       </MenuProvider>
     </div>
