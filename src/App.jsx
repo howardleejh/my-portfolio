79a460d0439813dedbe04c5f257d94a3b4c31257
Howard Lee <howardlee@Howards-MBP.local>
2022-06-19 00:31:10 +0800

    implemented routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MenuProvider from './Components/MenuProvider/MenuProvider'
 import AppLayout from './Components/AppLayout/AppLayout'
 import Home from './Pages/Home/Home'
+import Tokens from './Pages/Tokens/Tokens'
+import Nfts from './Pages/Nfts/Nfts'
+import Vault from './Pages/Vault/Vault'
+import Lottery from './Pages/Lottery/Lottery'
+import Roadmap from './Pages/Roadmap/Roadmap'
+import Faq from './Pages/Faq/Faq'
 import './App.scss'
 
 const App = () => {
@@ -10,7 +16,15 @@ const App = () => {
       <MenuProvider>
         <BrowserRouter>
           <AppLayout>
-            <Home />
+            <Routes>
+              <Route path='/' element={<Home />}></Route>
+              <Route path='tokens' element={<Tokens />}></Route>
+              <Route path='nft' element={<Nfts />}></Route>
+              <Route path='vault' element={<Vault />}></Route>
+              <Route path='lottery' element={<Lottery />}></Route>
+              <Route path='roadmap' element={<Roadmap />}></Route>
+              <Route path='faq' element={<Faq />}></Route>
+            </Routes>
           </AppLayout>
         </BrowserRouter>
       </MenuProvider>
