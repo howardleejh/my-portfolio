5726a2baa9a0e886d6fc8990338288e1203dd27c
Howard Lee <howardlee@Howards-MBP.local>
2022-09-02 20:35:55 +0800

    completed fix on dependencies issue

--- src/Components/WheelCarousel/WheelCarousel.jsx
+++ src/Components/WheelCarousel/WheelCarousel.jsx
@@ -1,85 +1,14 @@
 import { useRef, useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faHtml5,
-  faCss3Alt,
-  faReact,
-  faJsSquare,
-  faSass,
-  faBootstrap,
-  faNodeJs,
-  faEthereum,
-  faGitSquare,
-  faDigitalOcean,
-  faSpotify,
-} from '@fortawesome/free-brands-svg-icons'
-import {
   faChevronLeft,
   faChevronRight,
 } from '@fortawesome/free-solid-svg-icons'
 import { Row, Col } from 'antd'
 import { Fade } from 'react-awesome-reveal'
-import classnames from 'https://cdn.skypack.dev/classnames@2.3.1'
+import { images } from '../../Utilities/images'
+import classnames from 'classnames'
 import './WheelCarousel.scss'
-
-const images = [
-  {
-    icon: faHtml5,
-    title: 'HTML',
-    desc: `The fundamentals of it all, I learnt HTML Javascript and CSS during my time at General Assembly, I also picked up Search Engine Optimization using React Helmet with my projects to generate multiple document heads for better SEO performance.`,
-  },
-  {
-    icon: faCss3Alt,
-    title: 'CSS',
-    desc: `I work mainly with SASS as it is a very powerful CSS pre-processor. I believe in the separation of concerns design pattern and prefer to separate my CSS from my logic code.`,
-  },
-
-  {
-    icon: faSass,
-    title: 'SASS',
-    desc: `I use SASS but my file extensions are in SCSS. I like being able to tell apart blocks of code with curly braces. Also nest my selectors within relevant blocks gives so much visualization and clarity.`,
-  },
-  {
-    icon: faBootstrap,
-    title: 'Bootstrap',
-    desc: `Bootstrap used to be my go-to solution, I've since moved on to Ant Design, Bulma, Material UI and so much more. I've explored creating my own components to achieve more complex customizations and better reusability.`,
-  },
-  {
-    icon: faNodeJs,
-    title: 'NodeJS',
-    desc: `NodeJS is my main backend solution when creating Web2 based web applications. I work well with REST APIs and have recently picked up Firebase to add into my collection along with MySQL, MongoDB and Express.`,
-  },
-  {
-    icon: faGitSquare,
-    title: 'Git',
-    desc: `I use Git for version control and CI/CD pipeline deployments. My preferred choice for cloud based hosting would be Github though I use Gitlab in my professional working environments for better DevOps support.`,
-  },
-  {
-    icon: faReact,
-    title: 'React',
-    desc: `Yes, my main bread and butter. My main tech stack revolves around React and Javascript, I continue gaining experience with Gatsby for static website developments on the side. I am a functional components developer kind of guy.`,
-  },
-  {
-    icon: faJsSquare,
-    title: 'Javascript',
-    desc: `This is my primary language for web development, I have plans to pick up typescript some time soon but there's always something new to learn about with Javascript, my to-do list is never empty.`,
-  },
-  {
-    icon: faEthereum,
-    title: 'Solidity',
-    desc: `I use Solidity when developing smart contracts for EVM compatible blockchains, Open Zeppelin to adhere to the EIP and ERC standards, Chainlink for off-chain data processing, Mocha and Chai for unit testing and Tenderly for transaction debugging. Ethereum Remix online IDE for quick tests and drafting of smart contracts.`,
-  },
-  {
-    icon: faSpotify,
-    title: 'Spotify',
-    desc: `This is a definite essential, along with a cup of good caffeine, an awesome gaming chair and crazy LED lights. No I'm just kidding, but the general consensus is true!`,
-  },
-  {
-    icon: faDigitalOcean,
-    title: 'Digital Ocean',
-    desc: `My first production app was deployed in Digital Ocean. I also have experience with Vercel, Heroku, Github and Netlify just to name a few. These days I'm exploring Fleek.`,
-  },
-]
 
 const initialSlidesState = images.map((image, index) => ({
   coords: {
